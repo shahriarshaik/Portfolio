df160296e383d245e807b2c7ff2b6d786d6b03fd
ShakeZpear <meherss@uio.no>
2023-03-12 17:21:57 +0100

    done with navtest

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.querySelector('.navbar-toggle');
-
-    console.log(navbarToggle);
-
-    navbarToggle.addEventListener('click', () => {
-        navbarToggle.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active"); // Toggle hamburger icon
+        navMenu.classList.toggle("active"); // Toggle nav menu
     });
-
 });
