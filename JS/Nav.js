4cf8a2bbe5ab2ecc9ed0ae37eb69145b219af14b
ShakeZpear <meherss@uio.no>
2023-03-13 03:10:25 +0100

    fiksa nav i aboutme

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
+    const navMenu1 = document.querySelector(".nav-menu1");
     const navMenu = document.querySelector(".nav-menu");
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active"); // Toggle hamburger icon
+        navMenu1.classList.toggle("active"); // Toggle nav menu
         navMenu.classList.toggle("active"); // Toggle nav menu
     });
 });
